d6fc2fb40592fdef361dfb05df76ccdff39b6e2e
Chris <cf_anderson@netzero.com>
2019-07-30 15:07:37 -0500

    Json error message resolved

--- KanyeWestAPI/Program.cs
+++ KanyeWestAPI/Program.cs
@@ -9,11 +9,11 @@ namespace KanyeWestAPI
     {
         static void Main(string[] args)
         {
-            string url = "https://kanye.rest/";
+            string url = "https://api.kanye.rest/";
             HttpClient client = new HttpClient();
             string response = client.GetStringAsync(url).Result;
 
-            string joke2 = JObject.Parse(response).GetValue("value").ToString();
+            string joke2 = JObject.Parse(response).GetValue("quote").ToString();
 
             Console.WriteLine(joke2);
         }
