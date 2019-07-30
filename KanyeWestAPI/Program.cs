64bed7a85c7a7b03c6edd3a949e763ee51062a87
Chris <cf_anderson@netzero.com>
2019-07-29 19:32:16 -0500

    First Commit

---
+++ KanyeWestAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KanyeWestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://kanye.rest/";
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+
+            string joke2 = JObject.Parse(response).GetValue("value").ToString();
+
+            Console.WriteLine(joke2);
+        }
+    }
+}
